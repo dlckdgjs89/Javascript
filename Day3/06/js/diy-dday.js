b0eed0e4cbbd07033a1bde343d126ce4c3bb7108
dlckdgjs89 <dlckdgjs89@naver.com>
2020-04-07 17:47:44 +0900

    Day3

--- Day3/06/js/diy-dday.js
+++ Day3/06/js/diy-dday.js
@@ -11,3 +11,20 @@ console.log(passedDay);
 
 document.querySelector('#accent').innerHTML = 
     passedDay + "일";
+
+function calculator(days) {
+    var future = toFirst + days * (24*60*60*1000);
+    var someday = new Date(future);
+
+    var year = someday.getFullYear();
+    var month = someday.getMonth();
+    var date = someday.getDate();
+
+    document.querySelector('#date'+days).innerHTML =
+        year + "년 "+ month + "월 "+ date + "일";
+}
+
+calculator(100);
+calculator(200);
+calculator(365);
+calculator(500);
